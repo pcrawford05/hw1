d37f1de365a0fa03f621bcc29039b60dbbec88d4
Peyton Crawford <pcrawfor@usc.edu>
2025-01-20 09:36:27 +0000

    all done with part 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -4,10 +4,62 @@
 #include <sstream>
 
 #include "ulliststr.h"
-
+using namespace std;
 //Use this file to test your ulliststr implementation before running the test suite
 
 int main(int argc, char* argv[])
 {
+  ULListStr list;
+  list.push_back("oldfront");
+	list.push_back("oldback");
 
+	//EXPECT_TRUE(checkListContent(&list, {"oldfront", "oldback"}));
+
+	// this should create an empty slot at the front of the list, and the new element can be put there
+	list.pop_front();
+	list.push_front("newfront");
+  cout << "HELLOOOEOEOEOEOEOEEOO" << endl;
+	//EXPECT_TRUE(checkListContent(&list, {"newfront", "oldback"}));
+
+	// now, a new element struct should get created at the front
+	list.pop_back();
+	list.push_front("cloudfront");
+  cout << "HIIIIIYYIYYYIIIIII" << endl;
+  list.get(0);
+  list.get(1);
+
+	//EXPECT_TRUE(checkListContent(&list, {"cloudfront", "newfront"}));
+
+	// now, the original head should get deleted
+	list.pop_back();
+
+	//EXPECT_TRUE(checkListContent(&list, {"cloudfront"}));
+
+	list.pop_front();
+
+	//EXPECT_TRUE(checkListContent(&list, {}));
+
+	list.push_front("newback");
+  cout << "HEYYYYYYYEYEYEEYYEYYYYY" << endl;
+	//EXPECT_TRUE(checkListContent(&list, {"newback"}));
+
+  list.clear();
+
+/*
+  dat.push_back("XYZ?");
+  dat.pop_back();
+  dat.push_front("Def");
+  dat.pop_front();
+  dat.push_back("Ghi");
+  dat.push_front("Jkl");
+
+  dat.push_back("Mno");
+  dat.push_front("Qrs");
+  cout << dat.get(0) << " " << dat.get(6) << " " << dat.get(13) << endl;
+  dat.pop_back();
+  dat.pop_front();
+  cout << "Front: " << dat.front() << endl;
+  cout << "Back: " << dat.back() << endl;
+  */
+  return 0;
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -4,8 +4,8 @@
 
 ULListStr::ULListStr()
 {
-  head_ = NULL;
-  tail_ = NULL;
+  head_ = nullptr;
+  tail_ = nullptr;
   size_ = 0;
 }
 
@@ -21,15 +21,113 @@ bool ULListStr::empty() const
 
 size_t ULListStr::size() const
 {
-  return size_;
+  return size_; 
 }
 
 // WRITE YOUR CODE HERE
 
+void ULListStr::push_back(const std::string& val) {
+  if(head_ == nullptr){
+    head_ = tail_ = new Item();
+    head_->val[head_->last++] = val;
+  } else if(tail_->last == ARRSIZE){
+    tail_->next = new Item();
+    Item* temp = tail_;
+    tail_ = tail_->next;
+    tail_->prev = temp;
+    tail_->val[tail_->last++] = val;
+  } else {
+    tail_->val[tail_->last++] = val;
+  }
+  size_++;
+}
+
+void ULListStr::pop_back() {
+  if(head_ == nullptr)
+    return;
+  if(tail_->first == tail_->last - 1){
+    Item* temp = tail_;
+    if(tail_ == head_)
+      tail_ = head_ = nullptr;
+    else{
+      tail_ = tail_->prev;
+      if(head_ == tail_)
+        head_->next = nullptr;
+    }
+    delete temp;
+  } else {
+    tail_->val[--tail_->last] = "";
+  }
+  size_--;
+}
+  
+void ULListStr::push_front(const std::string& val) {
+  if(head_ == nullptr){
+    head_ = tail_ = new Item();
+    head_->val[head_->last++] = val;
+  } else if(head_->first == 0){
+    head_->prev = new Item();
+    Item* temp = head_;
+    head_ = head_->prev;
+    head_->next = temp;
+    head_->val[ARRSIZE - 1] = val;
+    head_->first = ARRSIZE - 1;
+    head_->last = ARRSIZE;
+  } else {
+    head_->val[--(head_->first)] = val;
+  }
+  size_++;
+}
+
+void ULListStr::pop_front() {
+  if(head_ == nullptr)
+    return;
+  if(head_->first == head_->last - 1){
+    Item* temp = head_;
+    if(tail_ == head_)
+      tail_ = head_ = nullptr;
+    else{
+      head_ = head_->next;
+      if(head_ == tail_)
+        tail_->prev = nullptr;
+    }
+    delete temp;
+  } else {
+    head_->val[head_->first++] = "";
+  }
+  size_--;
+}
+  
+std::string const & ULListStr::back() const {
+  if(head_ == nullptr)
+    throw std::invalid_argument("Can't access because does not exist.");
+  return tail_->val[tail_->last - 1];
+}
+
+std::string const & ULListStr::front() const {
+  if(head_ == nullptr)
+    throw std::invalid_argument("Can't access because does not exist.");
+  return head_->val[head_->first];
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const{
+  if(loc < 0 || loc >= size_ || head_ == nullptr)
+    return nullptr;
+  Item* t = head_;
+  while(true){
+    if(t->last - t->first <= loc){
+      loc -= t->last - t->first;
+      t = t->next;
+    } else {
+      return &t->val[t->first + loc];
+    }
+  }
+}
+
 void ULListStr::set(size_t loc, const std::string& val)
 {
   std::string* ptr = getValAtLoc(loc);
-  if(ptr == NULL){
+  if(ptr == nullptr){
     throw std::invalid_argument("Bad location");
   }
   *ptr = val;
@@ -38,7 +136,7 @@ void ULListStr::set(size_t loc, const std::string& val)
 std::string& ULListStr::get(size_t loc)
 {
   std::string* ptr = getValAtLoc(loc);
-  if(ptr == NULL){
+  if(ptr == nullptr){
     throw std::invalid_argument("Bad location");
   }
   return *ptr;
@@ -47,7 +145,7 @@ std::string& ULListStr::get(size_t loc)
 std::string const & ULListStr::get(size_t loc) const
 {
   std::string* ptr = getValAtLoc(loc);
-  if(ptr == NULL){
+  if(ptr == nullptr){
     throw std::invalid_argument("Bad location");
   }
   return *ptr;
@@ -55,11 +153,11 @@ std::string const & ULListStr::get(size_t loc) const
 
 void ULListStr::clear()
 {
-  while(head_ != NULL){
+  while(head_ != nullptr){
     Item *temp = head_->next;
     delete head_;
     head_ = temp;
   }
-  tail_ = NULL;
+  tail_ = nullptr;
   size_ = 0;
 }
